183c29540badcb4daf2d1eb327ad63bdf08f26ed
Maitrey Patel <mp458.njit.edu>
2017-03-08 17:49:59 -0500

    title change 1

--- display_discount.php
+++ display_discount.php
@@ -22,7 +22,7 @@ $product_description_escaped = htmlspecialchars($product_description);
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Product Discount</title>
+    <title>Product Discount Calculator</title>
     <link rel="stylesheet" type="text/css" href="main.css">
 </head>
 <body>
